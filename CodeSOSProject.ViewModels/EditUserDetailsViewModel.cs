8ff2236ee5f569012044f802ef537dd2893a2a7d
Felipe de Senco <55833207+Dijoig@users.noreply.github.com>
2021-07-31 09:48:12 +0100

    Added ViewModels Added References to Repository at ServiceLayer Added Reference to ViewModels at ServiceLayer and CodeSOS

---
+++ CodeSOSProject.ViewModels/EditUserDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeSOSProject.ViewModels
+{
+    public class EditUserDetailsViewModel
+    {       
+        [Required]
+        [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z]*$")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+    }
+}
